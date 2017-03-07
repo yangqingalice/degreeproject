b03abe83d37a9f0606c92a3359d998c2bf30b0b1
yangqingalice <qingyang@qings-mbp.risd.edu>
2017-03-07 14:10:47 -0500

    stone
    stone week1

--- stone/script.js
+++ stone/script.js
@@ -24,22 +24,5 @@ $('#track6').click(function(){
    $('#wrap').append('<embed id="embed_player" src="touch.mp3" autostart="true" hidden="true"></embed>');
 });
 
-$(".track").click(function(e){
-   var x = e.pageX - 20 + "px";
-   var y = e.pageY - 20 + "px";
-   var div = $("<div>").css({
-       "position": "absolute",
-       "left": x,
-       "top": y,
-       "height": "40px",
-       "width": "40px",
-       "background-color": "white",
-       "border-radius": "20px",
-       "opacity": "0.5"
-   });
-   $(document.body).append(div);
-   $("div").last().fadeOut("slow");
-
-});
 
 });
