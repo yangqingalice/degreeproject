f1ae624703f522248011be0c033ca4c7a0595e9f
yangqingalice <qingyang@qings-mbp.risd.edu>
2017-03-21 11:42:57 -0400

    change

--- sphere/script.js
+++ sphere/script.js
@@ -10,14 +10,14 @@ $(document).ready( function() {
   	window.ondevicemotion = function(e) {
   		ax = event.accelerationIncludingGravity.x * 5;
   		ay = event.accelerationIncludingGravity.y * 5;
-  		document.getElementById("accelerationX").innerHTML = e.accelerationIncludingGravity.x;
-  		document.getElementById("accelerationY").innerHTML = e.accelerationIncludingGravity.y;
-  		document.getElementById("accelerationZ").innerHTML = e.accelerationIncludingGravity.z;
+  		$("#accelerationX").innerHTML = e.accelerationIncludingGravity.x;
+  		$("#accelerationY").innerHTML = e.accelerationIncludingGravity.y;
+  		$("#accelerationZ").innerHTML = e.accelerationIncludingGravity.z;
 
   		if ( e.rotationRate ) {
-  			document.getElementById("rotationAlpha").innerHTML = e.rotationRate.alpha;
-  			document.getElementById("rotationBeta").innerHTML = e.rotationRate.beta;
-  			document.getElementById("rotationGamma").innerHTML = e.rotationRate.gamma;
+  			$("#rotationAlpha").innerHTML = e.rotationRate.alpha;
+  			$("#rotationBeta").innerHTML = e.rotationRate.beta;
+  			%("#rotationGamma").innerHTML = e.rotationRate.gamma;
   		}
   	}
 
@@ -51,7 +51,5 @@ $(document).ready( function() {
   	if (y>document.documentElement.clientHeight-20) { y = document.documentElement.clientHeight-20; vy = -vy; }
 
   }
-
-
 
 });
