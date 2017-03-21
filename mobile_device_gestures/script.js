b00d5593c8cb1f88bba0e5059eff10b08943aab8
yangqingalice <qingyang@qings-mbp.risd.edu>
2017-03-21 14:35:25 -0400

    dbl click link

--- mobile_device_gestures/script.js
+++ mobile_device_gestures/script.js
@@ -1,15 +1,9 @@
 $(document).ready(function() {
 
-    $("#circle").click(function(){
-      $(this).toggleClass("click");
-    });
 
     $("#circle").dblclick(function(){
       $(this).toggleClass("dblclick");
     });
 
-    $("#circle").swipe(function(){
-      $(this).toggleClass("swipe");
-    });
 
 });
